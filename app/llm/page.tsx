74d48250fe7d4bbaf421eb929329859da2d6536d
thaflavorsaver <dojohnson@ucdavis.edu>
2023-12-13 17:38:35 -0800

    removed unused imports on new pages

--- app/llm/page.tsx
+++ app/llm/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { IoIosCloseCircle } from "react-icons/io";
 import { HiMenu } from "react-icons/hi";
 
